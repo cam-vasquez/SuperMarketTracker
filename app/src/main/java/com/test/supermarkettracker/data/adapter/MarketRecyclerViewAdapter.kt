791e8673b4672acaf76acd59bfebe72919983ab2
cam-vasquez <00065221@uca.edu.sv>
2023-06-04 22:57:41 -0600

    add recyclerview

---
+++ app/src/main/java/com/test/supermarkettracker/data/adapter/MarketRecyclerViewAdapter.kt
@@ -0,0 +1,31 @@
+package com.test.supermarkettracker.data.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.test.supermarkettracker.data.model.MarketModel
+import com.test.supermarkettracker.databinding.FragmentItemMarketBinding
+
+class MarketRecyclerViewAdapter(
+    private val clickListener: (MarketModel) -> Unit
+): RecyclerView.Adapter<MarketRecyclerViewHolder>() {
+
+    private val markets = ArrayList<MarketModel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarketRecyclerViewHolder {
+        val binding = FragmentItemMarketBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MarketRecyclerViewHolder(binding)
+    }
+
+    override fun getItemCount() = markets.size
+
+    override fun onBindViewHolder(holder: MarketRecyclerViewHolder, position: Int) {
+        val market = markets[position]
+        holder.bind(market, clickListener)
+    }
+
+    fun setData(marketList: List<MarketModel>){
+        markets.clear()
+        markets.addAll(marketList)
+    }
+}
